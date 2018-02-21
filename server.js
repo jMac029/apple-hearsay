615ad31b08b8da42afe5a89c471face82a4026e9
James McMillan <jmcmillan29@gmail.com>
2018-02-21 12:56:41 -0800

    changing port for heroku to be happy and deploy my app

--- server.js
+++ server.js
@@ -171,6 +171,6 @@ app.post('/articles/:id', function(req, res) {
 
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(process.env.PORT || PORT, function() {
     console.log('App running on port ' + PORT + '!');
 })
